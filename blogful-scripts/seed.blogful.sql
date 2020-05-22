3e3042194a36b4aee2adb2d0da2dacccdc272f65
JTLiner925 <[jtliner925@gmail.com]>
2020-05-22 11:48:55 -0500

    finished exercises

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+('first article', 'this is my content', '2019-08-02 15:10:25-07'),
+('my next article', 'content content content', '2019-08-02 15:10:25-07'),
+('third article', 'this is my content', '2019-10-12 08:16:25-07'),
+('my fourth article', 'content content content', '2019-08-02 15:10:25-07'),
+('fifth article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my sixth article', 'content content content', '2019-08-02 15:10:25-07'),
+('seventh article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my eighth article', 'content content content', '2019-08-02 15:10:25-07'),
+('ninth article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my tenth article', 'content content content', '2019-08-02 15:10:25-07'),
+('eleventh article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my twelvth article', 'content content content', '2019-08-02 15:10:25-07'),
+('thirteenth article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my fourteeth article', 'content content content', '2019-08-02 15:10:25-07'),
+('fifteenth article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my sixteenth article', 'content content content', '2019-08-02 15:10:25-07'),
+('seventeenth article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my eighteenth article', 'content content content', '2019-08-02 15:10:25-07'),
+('nineteenth article', 'this is my content', '2019-06-22 19:10:25-07'),
+('my twentieth article', 'content content content', '2019-08-02 15:10:25-07');
+
+COMMIT;
